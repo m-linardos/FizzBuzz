5328d8165c85495fa0c622ae379a15c084a44879
Marianne <marianneklinardos@gmail.com>
2020-01-25 14:46:55 -0500

    Add files via upload

---
+++ 12-FizzBuzz/12-FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPG_12_FizzBuzz {
+	class Program {
+		static void Main(string[] args) {
+			int num = 0;
+			Console.WriteLine("FizzBuzz");
+			Console.WriteLine();
+			// Loop through #s 1-100
+			for(num = 1; num < 101; num++) 
+			{					
+				if(num % 3 == 0) {
+					Console.Write("Fizz");						
+				}
+				 else if(num % 5 == 0) {
+					Console.Write("Buzz");
+				 }
+				 else if(num % 3 == 0 && num % 5 == 0) {
+					Console.Write("FizzBuzz");
+				 }
+				 else {
+					Console.Write(num);
+				 }
+				Console.Write(" ");
+			}
+			Console.ReadKey();
+
+		}
+	}
+}
